274015a5d8ac601978b0036f1e73cf8e6988274e
Juda Escalera Flores <juda.escaleraflores@credencialprocessing.com>
2021-07-27 23:34:42 -0500

    add framework

---
+++ CardData.framework/Headers/CardData.h
@@ -0,0 +1,18 @@
+//
+//  CardData.h
+//  CardData
+//
+//  Created by Juda Escalera on 26/07/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CardData.
+FOUNDATION_EXPORT double CardDataVersionNumber;
+
+//! Project version string for CardData.
+FOUNDATION_EXPORT const unsigned char CardDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CardData/PublicHeader.h>
+
+
